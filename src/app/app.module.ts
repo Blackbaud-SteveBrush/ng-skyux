76f4d7ac99829a535bca4c1d62d69549dd7f99cc
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2019-04-23 17:25:56 -0400

    Use dummy module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { SkyPagingModule } from '@skyux/lists';
+import { SkyPagingModule } from 'stevebrush-skyux-lists-test';
 
 @NgModule({
   declarations: [

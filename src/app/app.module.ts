39a1817d555a11f2ea634a714f5a6acfd93afdbc
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2019-04-22 20:58:44 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SkyPagingModule } from '@skyux/lists';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SkyPagingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
